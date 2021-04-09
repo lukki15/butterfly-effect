81e795ac134b6646d09e3a758d1bafe041034078
lukki15 <50550773+lukki15@users.noreply.github.com>
2021-04-09 15:44:49 +0200

    add multi target (wasm/native)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::build();
+    app.insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins);
+    #[cfg(target_arch = "wasm32")]
+    app.add_plugin(bevy_webgl2::WebGL2Plugin);
+    app.add_startup_system(hello_word.system()).run();
+}
+
+fn hello_word(){
+    println!("hello world");
 }
