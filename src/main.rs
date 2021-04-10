7397d348352ba588b3b9614f66fe50a2eb5f5e4c
lukki15 <50550773+lukki15@users.noreply.github.com>
2021-04-10 12:49:27 +0200

    collide with wall

--- src/main.rs
+++ src/main.rs
@@ -50,6 +50,7 @@ enum Direction {
     Up,
     Right,
     Down,
+    StandStill,
 }
 
 impl Direction {
@@ -59,6 +60,7 @@ impl Direction {
             Self::Right => Self::Left,
             Self::Up => Self::Down,
             Self::Down => Self::Up,
+            Self::StandStill => Self::StandStill,
         }
     }
 }
@@ -91,16 +93,12 @@ fn spawn_target(
             sprite: Sprite::new(Vec2::new(SPRITE_WIDTH as f32, SPRITE_HEIGHT as f32)),
             ..Default::default()
         })
-        .insert(Wall {})
+        .insert(Target {})
         .insert(wall_position)
         .insert(Size::square(0.9));
 }
 
-fn setup(
-    mut commands: Commands,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    asset_server: Res<AssetServer>,
-) {
+fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     let level_data: Vec<String> = vec![
         "          WWWW        ".to_string(),
         "   WWWW   WWWW        ".to_string(),
@@ -164,18 +162,9 @@ fn setup(
                 y: y as i32 + 1,
             };
             if c == 'W' {
-                spawn_wall(
-                    &mut commands,
-                    &mut materials,
-                    wall_color,
-                    pos,
-                );
+                spawn_wall(&mut commands, &mut materials, wall_color, pos);
             } else if c == 'T' {
-                spawn_target(
-                    &mut commands,
-                    &mut materials,
-                    pos,
-                );
+                spawn_target(&mut commands, &mut materials, pos);
             }
         }
     }
@@ -194,56 +183,88 @@ fn spawn_rocket(
             ..Default::default()
         })
         .insert(Rocket {
-            direction: Direction::Up,
+            direction: Direction::StandStill,
         })
-        .insert(Position { x: 3, y: 3 })
+        .insert(Position { x: 1, y: 1 })
         .insert(Size::square(0.8))
         .id();
 }
 
-fn snake_movement_input(keyboard_input: Res<Input<KeyCode>>, mut heads: Query<&mut Rocket>) {
-    if let Some(mut head) = heads.iter_mut().next() {
-        let dir: Direction = if keyboard_input.pressed(KeyCode::Left) {
+fn rocket_movement_input(keyboard_input: Res<Input<KeyCode>>, mut rockets: Query<&mut Rocket>) {
+    let right = keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D);
+    let left = keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A);
+    let up = keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W);
+    let down = keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S);
+
+    if let Some(mut rocket) = rockets.iter_mut().next() {
+        let dir: Direction = if left {
             Direction::Left
-        } else if keyboard_input.pressed(KeyCode::Down) {
+        } else if down {
             Direction::Down
-        } else if keyboard_input.pressed(KeyCode::Up) {
+        } else if up {
             Direction::Up
-        } else if keyboard_input.pressed(KeyCode::Right) {
+        } else if right {
             Direction::Right
         } else {
-            head.direction
+            rocket.direction
         };
-        if dir != head.direction.opposite() {
-            head.direction = dir;
+        if dir != rocket.direction.opposite() {
+            rocket.direction = dir;
         }
     }
 }
+// Query<(&mut Position, &Rocket)>, Query<&Position, With<Wall>>
+fn rocket_movement(
+    mut rocket_query: Query<(&Rocket, &mut Position)>,
+    collider_query: Query<&Transform, With<Wall>>,
+    windows: Res<Windows>
+) {
+    let window = windows.get_primary().unwrap();
+    if let Some((rocket, mut rocket_pos)) = rocket_query.iter_mut().next() {
+        let mut next_position = rocket_pos.clone();
 
-fn snake_movement(mut heads: Query<(&mut Position, &Rocket)>) {
-    if let Some((mut head_pos, head)) = heads.iter_mut().next() {
-        match &head.direction {
+        match &rocket.direction {
             Direction::Left => {
-                if head_pos.x > 0 {
-                    head_pos.x -= 1;
+                if rocket_pos.x > 0 {
+                    next_position.x -= 1;
                 }
             }
             Direction::Right => {
-                if head_pos.x < ARENA_WIDTH as i32 - 1 {
-                    head_pos.x += 1;
+                if rocket_pos.x < ARENA_WIDTH as i32 - 1 {
+                    next_position.x += 1;
                 }
             }
             Direction::Up => {
-                if head_pos.y < ARENA_HEIGHT as i32 - 1 {
-                    head_pos.y += 1;
+                if rocket_pos.y < ARENA_HEIGHT as i32 - 1 {
+                    next_position.y += 1;
                 }
             }
             Direction::Down => {
-                if head_pos.y > 0 {
-                    head_pos.y -= 1;
+                if rocket_pos.y > 0 {
+                    next_position.y -= 1;
                 }
             }
+            Direction::StandStill => {}
         };
+
+        let mut no_collision = true;
+        
+        for wall_transform in collider_query.iter(){
+            
+            let next_transform_x = convert_x(next_position.x, window.width());
+            let next_transform_y = convert_y(
+                next_position.y ,
+                window.height()
+            );
+            if wall_transform.translation.x == next_transform_x && wall_transform.translation.y == next_transform_y{
+                no_collision = false;
+            }
+        }
+        
+        if no_collision {
+            rocket_pos.x = next_position.x;
+            rocket_pos.y = next_position.y;
+        }
     }
 }
 
@@ -258,19 +279,32 @@ fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Sprite)>) {
     }
 }
 
+fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+    let tile_size = bound_window / bound_game;
+    pos / bound_game * bound_window - (bound_window / 2.) + (tile_size / 2.)
+}
+
+fn convert_x(pos: i32, window_width: f32) -> f32{
+    convert(pos as f32, window_width, ARENA_WIDTH as f32)
+}
+
+fn convert_y(pos: i32, window_height: f32) -> f32{
+    convert(
+        pos as f32,
+        window_height,
+        (ARENA_HEIGHT + SCORE_BOARD_HEIGHT) as f32,
+    )
+}
+
 fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Transform)>) {
-    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
-        let tile_size = bound_window / bound_game;
-        pos / bound_game * bound_window - (bound_window / 2.) + (tile_size / 2.)
-    }
+
     let window = windows.get_primary().unwrap();
     for (pos, mut transform) in q.iter_mut() {
         transform.translation = Vec3::new(
-            convert(pos.x as f32, window.width() as f32, ARENA_WIDTH as f32),
-            convert(
-                pos.y as f32,
-                window.height() as f32,
-                (ARENA_HEIGHT + SCORE_BOARD_HEIGHT) as f32,
+            convert_x(pos.x, window.width()),
+            convert_y(
+                pos.y,
+                window.height()
             ),
             0.0,
         );
@@ -289,7 +323,7 @@ fn main() {
         .add_startup_system(setup.system())
         .add_startup_stage("game_setup", SystemStage::single(spawn_rocket.system()))
         .add_system(
-            snake_movement_input
+            rocket_movement_input
                 .system()
                 .label(SnakeMovement::Input)
                 .before(SnakeMovement::Movement),
@@ -297,7 +331,7 @@ fn main() {
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(0.050))
-                .with_system(snake_movement.system().label(SnakeMovement::Movement)),
+                .with_system(rocket_movement.system().label(SnakeMovement::Movement)),
         )
         .add_system_set_to_stage(
             CoreStage::PostUpdate,
