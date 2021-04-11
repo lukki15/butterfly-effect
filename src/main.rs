39055c00bc6abfbdc995304d87edc32817613885
lukki15 <50550773+lukki15@users.noreply.github.com>
2021-04-11 17:22:33 +0200

    add status bar in code

--- src/main.rs
+++ src/main.rs
@@ -463,6 +463,29 @@ fn setup_scoreboard(
         });
 }
 
+fn _setup_statusbar(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+){
+    let texture_handle = asset_server.load("status_bar.png");
+        let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(32.0, 8.0), 4, 1);
+        let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+        commands
+        .spawn_bundle(SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            sprite: TextureAtlasSprite {
+                index: 0,
+                ..Default::default()
+            },
+            ..Default::default()
+        }).insert(Position {
+            x: ARENA_WIDTH as i32 / 2,
+            y: ARENA_HEIGHT as i32 + 1 ,
+        });
+}
+
 fn setup(
     mut commands: Commands,
     mut materials: ResMut<Assets<ColorMaterial>>,
